00cba7101d5a72cd99537905195cc39dd7b548b9
Marco Bruno <marco.bruno.br@gmail.com>
2019-06-25 23:19:11 +0100

    Feito o header usando flex

--- src/App.js
+++ src/App.js
@@ -1,17 +1,9 @@
 import React from 'react';
 
-import Logo from './objects/Logo';
-import Sobre from './objects/Sobre';
-import Menu from './objects/Menu';
+import Header from './components/Header';
 
 function App() {
-  return (
-    <>
-      <Logo />
-      <Sobre />
-      <Menu />
-    </>
-  );
+  return (<Header />);
 }
 
 export default App;
