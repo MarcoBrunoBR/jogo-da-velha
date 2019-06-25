2ca127e41473a8f6b839a27afff6dd96677c5b18
Marco Bruno <marco.bruno.br@gmail.com>
2019-06-25 22:43:07 +0100

    Criado o object Menu

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 
 import Logo from './objects/Logo';
 import Sobre from './objects/Sobre';
+import Menu from './objects/Menu';
 
 function App() {
   return (
     <>
       <Logo />
       <Sobre />
+      <Menu />
     </>
   );
 }
