94bc4671020ab04c49ab4fedd1dc265124e96edb
Marco Bruno <marco.bruno.br@gmail.com>
2019-07-08 19:28:54 +0100

    Criado o object Label

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 
 import Card from './objects/Card';
+import Label from './objects/Label';
 import Header from './components/Header';
 import Hashtag from './components/Hashtag';
 
@@ -12,6 +13,7 @@ function App() {
       <Card>
         <Hashtag />
       </Card>
+      <Label content="Mostrar eventos" />
     </>
   );
 }
