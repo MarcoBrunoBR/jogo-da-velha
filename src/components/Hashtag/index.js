c388052a868d63b16f5b49dd527d4d573652ff42
Marco Bruno <marco.bruno.br@gmail.com>
2019-07-25 11:16:29 +0100

    Finalizado o hashtag

--- src/components/Hashtag/index.js
+++ src/components/Hashtag/index.js
@@ -1,22 +1,40 @@
-import React from 'react';
-import './styles.css';
+import React from "react";
+import "./styles.css";
 
-import Player from '../../objects/Player';
+import Player from "../../objects/Player";
 
 const Hashtag = () => (
-    <ul className="hashtag">
-        <li><Player /></li>
-        <li><Player /></li>
-        <li><Player /></li>
+  <ul className="hashtag">
+    <li class="item">
+      <Player />
+    </li>
+    <li class="item">
+      <Player />
+    </li>
+    <li class="item">
+      <Player />
+    </li>
 
-        <li><Player /></li>
-        <li><Player /></li>
-        <li><Player /></li>
-        
-        <li><Player /></li>
-        <li><Player /></li>
-        <li><Player /></li>
-    </ul>
+    <li class="item">
+      <Player />
+    </li>
+    <li class="item">
+      <Player />
+    </li>
+    <li class="item">
+      <Player />
+    </li>
+
+    <li class="item">
+      <Player />
+    </li>
+    <li class="item">
+      <Player />
+    </li>
+    <li class="item">
+      <Player />
+    </li>
+  </ul>
 );
 
 export default Hashtag;
