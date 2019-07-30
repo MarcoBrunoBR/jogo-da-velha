c304e51490bdc61eab66e7c75640eee77a94f469
Marco Bruno <marco.bruno.br@gmail.com>
2019-07-30 17:06:16 +0100

    Created the component IconSocial

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import React from "react";
 import "./App.css";
 import Profile from "./img/profile.jpg";
+import GitHub from "./img/github.png";
 
 import Card from "./objects/Card";
 import Checkbox from "./objects/Checkbox";
 import About from "./objects/About";
 import AboutAvatar from "./objects/AboutAvatar";
 import TitleGame from "./objects/TitleGame";
+import DescriptionGame from "./objects/DescriptionGame";
+import IconSocial from "./objects/IconSocial";
 import Header from "./components/Header";
 import Hashtag from "./components/Hashtag";
 import HeaderInternal from "./components/HeaderInternal";
-import DescriptionGame from "./objects/DescriptionGame";
 
 function App() {
   return (
@@ -25,6 +27,8 @@ function App() {
         <AboutAvatar src={Profile} alt="Profile do Marco Bruno" />
         <TitleGame content="Marco Bruno" />
         <DescriptionGame content="Migrei de palhaço para Dev. Front-End/UX e agora eu trabalho como streamer de código, instrutor e dev na CollabCode" />
+
+        <IconSocial src={GitHub} alt="Logo do GitHub" />
       </About>
     </>
   );
