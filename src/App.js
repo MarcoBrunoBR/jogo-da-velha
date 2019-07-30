2e1c70ba7a7989647bdd7a699ad9c783e79514ea
Marco Bruno <marco.bruno.br@gmail.com>
2019-07-30 18:17:46 +0100

    Refactory and created the ProfileUser

--- src/App.js
+++ src/App.js
@@ -1,17 +1,14 @@
 import React from "react";
 import "./App.css";
-import Profile from "./img/profile.jpg";
+import Photo from "./img/profile.jpg";
 
 import Card from "./objects/Card";
 import Checkbox from "./objects/Checkbox";
 import About from "./objects/About";
-import AboutAvatar from "./objects/AboutAvatar";
-import TitleGame from "./objects/TitleGame";
-import DescriptionGame from "./objects/DescriptionGame";
 import Header from "./components/Header";
 import Hashtag from "./components/Hashtag";
 import HeaderInternal from "./components/HeaderInternal";
-import SocialMedia from "./objects/SocialMedias";
+import ProfileUser from "./components/ProfileUser";
 
 function App() {
   return (
@@ -23,11 +20,7 @@ function App() {
       <Checkbox id="show" value="Mostrar eventos" />
       <About>
         <HeaderInternal />
-        <AboutAvatar src={Profile} alt="Profile do Marco Bruno" />
-        <TitleGame content="Marco Bruno" />
-        <DescriptionGame content="Migrei de palhaço para Dev. Front-End/UX e agora eu trabalho como streamer de código, instrutor e dev na CollabCode" />
-
-        <SocialMedia />
+        <ProfileUser photo={Photo} />
       </About>
     </>
   );
--- src/objects/AvatarProfile/index.js
+++ src/objects/AvatarProfile/index.js
@@ -1,8 +1,8 @@
 import React from "react";
 import "./styles.css";
 
-const AboutAvatar = ({ src, alt }) => (
+const AvatarProfile = ({ src, alt }) => (
   <img className="about-avatar" src={src} alt={alt} />
 );
 
-export default AboutAvatar;
+export default AvatarProfile;
