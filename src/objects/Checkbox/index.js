c6c60ba1224723cb22bdc11d5ef3da1321bd2a4a
Marco Bruno <marco.bruno.br@gmail.com>
2019-08-02 12:21:07 +0100

    Refactoring object Input para InputGame

--- src/objects/Checkbox/index.js
+++ src/objects/Checkbox/index.js
@@ -1,12 +1,12 @@
 import React from "react";
 import "./styles.css";
 
-import Input from "../Input";
+import InputGame from "../InputGame";
 import Label from "../Label";
 
 const Checkbox = ({ id, value }) => (
   <>
-    <Input id={id} value={value} type="checkbox" />
+    <InputGame id={id} value={value} type="checkbox" />
     <Label htmlFor={id} content={value}>
       <span className="beautiful"></span>
     </Label>
